864732d71bf02a19e81cae42bafcf43d177aa46d
Christian Chung <christian.m.chung@gmail.com>
2020-06-03 12:43:11 -0400

    Add date tooltips

--- includes/classes/Meta/Dates.php
+++ includes/classes/Meta/Dates.php
@@ -61,16 +61,19 @@ class Dates {
 				'id'   => 'openDate',
 				'name' => __( 'Grant Open', 'ca-grants-plugin' ),
 				'type' => 'datetime-local',
+				'description' => __( 'Please indicate when the grant should be made available in full on the portal (Open Date).', 'ca-grants-plugin' ),
 			),
 			array(
 				'id'   => 'closeDate',
 				'name' => __( 'Grant Close', 'ca-grants-plugin' ),
 				'type' => 'datetime-local',
+				'description' => __( 'Please indicate when the grant should be archived on the portal (Close Date). (Archived grants are still accessible on the portal after their Close Dates, but are marked as Closed and not included in search results.)<br/><br/>If the grant does not have a Close Date, that field may be left blank. Grants without Close Dates remain active and accessible via search results until updated with a Close Date.', 'ca-grants-plugin' ),
 			),
 			array(
 				'id'   => 'periodOfPerformance',
 				'name' => __( 'Period of Performance', 'ca-grants-plugin' ),
 				'type' => 'period-performance',
+				'description' => __( 'What is the total length of time that the award is available and active (i.e. do recipients have access to grant funds only within a specific timeframe)?', 'ca-grants-plugin' ),
 			),
 			array(
 				'id'   => 'expectedAwardDate',
