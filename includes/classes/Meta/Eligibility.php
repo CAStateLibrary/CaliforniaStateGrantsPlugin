8fde57051a7c49fd0e4e81909ff2663323ac54ed
Christian Chung <christian.m.chung@gmail.com>
2020-06-03 12:33:48 -0400

    Add eligibility toolitps

--- includes/classes/Meta/Eligibility.php
+++ includes/classes/Meta/Eligibility.php
@@ -58,22 +58,26 @@ class Eligibility {
 	public static function get_fields() {
 		return array(
 			array(
-				'id'     => 'applicantType',
-				'name'   => __( 'Eligibility: Applicant Type', 'ca-grants-plugin' ),
-				'type'   => 'checkbox',
-				'source' => 'api',
+				'id'          => 'applicantType',
+				'name'        => __( 'Eligibility: Applicant Type', 'ca-grants-plugin' ),
+				'type'        => 'checkbox',
+				'source'      => 'api',
+				'description' => __( 'Please indicate the applicant type(s) eligible to apply for this grant opportunity.<br/><br/>Note: Non-profits do not need to have 501(c)(3) status; Public agency can be state, county, city, town, or special district, and can also be K-12, college or university affiliated; Tribal nation groups can be federally recognized and/or state recognised, or have no government recognition.', 'ca-grants-plugin' ),
 			),
 			array(
-				'id'         => 'applicantTypeNotes',
-				'name'       => __( 'Applicant Type Notes', 'ca-grants-plugin' ),
-				'type'       => 'textarea',
-				'text_limit' => 250,
+				'id'          => 'applicantTypeNotes',
+				'name'        => __( 'Applicant Type Notes', 'ca-grants-plugin' ),
+				'type'        => 'textarea',
+				'text_limit'  => 250,
+				'description' => __( 'If applicable, include any clarifications or additional information regarding applicant type eligibility.', 'ca-grants-plugin' ),
+
 			),
 			array(
-				'id'         => 'geoLimitations',
-				'name'       => __( 'Eligibility: Geographic', 'ca-grants-plugin' ),
-				'type'       => 'textarea',
-				'text_limit' => 450,
+				'id'          => 'geoLimitations',
+				'name'        => __( 'Eligibility: Geographic', 'ca-grants-plugin' ),
+				'type'        => 'textarea',
+				'text_limit'  => 450,
+				'description' => __( 'If applicable, provide details on any geographic requirements, limitations, or exclusions.<br/><br/>Must applicants live or do business in a specified geographic area? Does the grant money have to be spent only in certain geographic areas, or are there any focused priorities, such as disadvantage communities?', 'ca-grants-plugin' ),
 			),
 		);
 	}
