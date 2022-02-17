529022d94f585db3577179455cb6ba2a10575cf4
Sudar Muthu <sudar@sudarmuthu.com>
2022-02-17 19:33:13 +0530

    If the plugin is used in the portal then don't show the taxonomy fields

--- includes/classes/Meta/Funding.php
+++ includes/classes/Meta/Funding.php
@@ -7,6 +7,8 @@
 
 namespace CaGov\Grants\Meta;
 
+use function CaGov\Grants\Core\is_portal;
+
 /**
  * Funding Grant Data Meta Class
  */
@@ -56,68 +58,80 @@ class Funding {
 	 * @return array
 	 */
 	public static function get_fields() {
-		return array(
-			array(
-				'id'          => 'fundingSource',
-				'name'        => __( 'Funding Source', 'ca-grants-plugin' ),
-				'type'        => 'radio',
-				'source'      => 'api',
-				'description' => __( 'If you select "Other", please elaborate in the funding source notes field below.', 'ca-grants-plugin' ),
-				'required'    => array( 'active' ),
-			),
-			array(
+		$fields = [];
+
+		if ( ! is_portal() ) {
+			$fields[] = array(
+					'id'          => 'fundingSource',
+					'name'        => __( 'Funding Source', 'ca-grants-plugin' ),
+					'type'        => 'radio',
+					'source'      => 'api',
+					'description' => __( 'If you select "Other", please elaborate in the funding source notes field below.', 'ca-grants-plugin' ),
+					'required'    => array( 'active' ),
+			);
+		}
+
+		$fields[] = array(
 				'id'          => 'revenueSourceNotes',
 				'name'        => __( 'Funding Source Notes', 'ca-grants-plugin' ),
 				'type'        => 'textarea',
 				'text_limit'  => 200,
 				'description' => __( 'Please specify information that may be helpful to applicants, such as proposition number, bond name/number, federal grant program, etc.', 'ca-grants-plugin' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'       => 'matchingFunds',
 				'name'     => __( 'Eligibility: Matching Funds', 'ca-grants-plugin' ),
 				'type'     => 'eligibility-matching-funds',
 				'required' => array( 'active' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'          => 'matchingFundsNotes',
 				'name'        => __( 'Matching Funds Notes', 'ca-grants-plugin' ),
 				'type'        => 'textarea',
 				'text_limit'  => 450,
 				'description' => __( 'Please provide any additional details and/or exceptions, or requirements around acceptable matched funding sources (e.g. public or private funding only). If your grant has no requirement for matching funds, but gives preference based on applications that have them, please indicate this in your Grant Description.', 'ca-grants-plugin' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'       => 'estimatedAvailableFunds',
 				'name'     => __( 'Total Estimated Available Funding', 'ca-grants-plugin' ),
 				'type'     => 'number',
 				'required' => array( 'active' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'       => 'estimatedAwards',
 				'name'     => __( 'Estimated Number of Awards', 'ca-grants-plugin' ),
 				'type'     => 'estimated-number-awards',
 				'required' => array( 'active' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'       => 'estimatedAmounts',
 				'name'     => __( 'Estimated Award Amounts', 'ca-grants-plugin' ),
 				'type'     => 'estimated-award-amounts',
 				'required' => array( 'active' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'          => 'disbursementMethod',
 				'name'        => __( 'Disbursement Method', 'ca-grants-plugin' ),
 				'type'        => 'radio',
 				'source'      => 'api',
 				'description' => __( 'If you select "Other", please elaborate in the funding method notes field below.', 'ca-grants-plugin' ),
 				'required'    => array( 'active' ),
-			),
-			array(
+		);
+
+		$fields[] = array(
 				'id'          => 'disbursementMethodNotes',
 				'name'        => __( 'Funding Method Notes', 'ca-grants-plugin' ),
 				'type'        => 'textarea',
 				'text_limit'  => 450,
 				'description' => __( 'Use this field to include 1-2 sentences providing details on the funding method.', 'ca-grants-plugin' ),
-			),
 		);
+
+		return $fields;
 	}
 }
