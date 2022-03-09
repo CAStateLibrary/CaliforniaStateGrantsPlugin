20664f2183d062ffde8b73dc2fa4b1bc473bdc51
Rahul Prajapati <rahul.prajapati@live.in>
2022-03-09 18:28:43 +0530

    E-4.3.5 - Update grant REST api for awardstats data.

--- includes/classes/Meta/AwardStats.php
+++ includes/classes/Meta/AwardStats.php
@@ -8,6 +8,7 @@
 namespace CaGov\Grants\Meta;
 
 use CaGov\Grants\Core;
+use WP_Error;
 
 /**
  * General Grant Data Meta Class
@@ -66,19 +67,23 @@ class AwardStats {
 	 * @return array
 	 */
 	public static function get_fields() {
-		$post_id          = get_the_ID();
-		$isForecasted     = get_post_meta( $post_id, 'isForecasted', true );
-
-		if ( empty( $isForecasted ) ) {
-			return array();
-		}
-
-		$is_ongoing_grant = Core\is_ongoing_grant( $post_id );
-		$deadline         = get_post_meta( $post_id, 'deadline', true );
 		$fiscal_year      = '';
+		$is_ongoing_grant = true;
 
-		if ( ! $is_ongoing_grant && ! empty( $deadline ) ) {
-			$fiscal_year = Core\get_fiscal_year( gmdate( 'Y-m-d H:m:s', $deadline ) );
+		if ( is_admin() ) {
+			$post_id      = get_the_ID();
+			$isForecasted = get_post_meta( $post_id, 'isForecasted', true );
+
+			if ( empty( $isForecasted ) ) {
+				return array();
+			}
+
+			$is_ongoing_grant = Core\is_ongoing_grant( $post_id );
+			$deadline         = get_post_meta( $post_id, 'deadline', true );
+
+			if ( ! $is_ongoing_grant && ! empty( $deadline ) ) {
+				$fiscal_year = Core\get_fiscal_year( gmdate( 'Y-m-d H:m:s', $deadline ) );
+			}
 		}
 
 		$fields = array(
@@ -113,7 +118,6 @@ class AwardStats {
 				'name'          => __( 'Grant award stats data:', 'ca-grants-plugin' ),
 				'type'          => 'group',
 				'description'   => __( 'Grant award stats data:', 'ca-grants-plugin' ),
-				'required'      => array( 'active' ),
 				'serialize'     => true,
 				'add_new_label' => __( 'Add Fiscal Year', 'ca-grants-plugin' ),
 				'is_multiple'   => Core\is_ongoing_grant( $post_id ),
@@ -122,5 +126,64 @@ class AwardStats {
 		);
 
 		return $group_fields;
+	}
+
+	/**
+	 * Validate award stats data for grant and send error if invalid data found.
+	 *
+	 * @param array $award_stats Grant award stats data.
+	 * @param int   $grant_id Grant post ID.
+	 *
+	 * @return WP_Error|boolean
+	 */
+	public static function get_validation_errors( $award_stats, $grant_id ) {
+		$errors = new WP_Error();
+
+		if ( empty( $award_stats ) || ! is_array( $award_stats ) ) {
+			$errors->add(
+				'validation_error',
+				esc_html__( 'Invalid or empty awardStats data, please check and try again.', 'ca-grants-plugin' )
+			);
+			return $errors;
+		}
+
+		$isForecasted = get_post_meta( $grant_id, 'isForecasted', true );
+
+		if ( empty( $isForecasted ) ) {
+			$errors->add(
+				'validation_error',
+				esc_html__( 'Grant type is not defined, awardStats value can\'t be accepted. Please update grant type and try again.', 'ca-grants-plugin' )
+			);
+			return $errors;
+		}
+
+		$is_ongoing_grant = Core\is_ongoing_grant( $grant_id );
+
+		// We only need one fiscal year data for closed grants.
+		if ( ! $is_ongoing_grant && count( $award_stats ) > 1 ) {
+			$errors->add(
+				'validation_error',
+				esc_html__( 'Invalid awardStats data: grant is closed and can have only one fiscal year data. Multiple entry found.', 'ca-grants-plugin' )
+			);
+			return $errors;
+		}
+
+		$deadline    = get_post_meta( $grant_id, 'deadline', true );
+		$fiscal_year = empty( $deadline ) ? '' : Core\get_fiscal_year( gmdate( 'Y-m-d H:m:s', $deadline ) );
+
+		if (
+			! $is_ongoing_grant
+			&& ! empty( $fiscal_year )
+			&& ! empty( $award_stats[0]['fiscalYear'] )
+			&& $fiscal_year !== $award_stats[0]['fiscalYear']
+		) {
+			$errors->add(
+				'validation_error',
+				esc_html__( 'Invalid fiscalYear found in awardStats data: Grant is closed and fiscal year should match with deadline date.', 'ca-grants-plugin' )
+			);
+			return $errors;
+		}
+
+		return true;
 	}
 }
