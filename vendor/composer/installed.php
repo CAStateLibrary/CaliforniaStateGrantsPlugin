6964c3dac0f791b58479c537551b10aff203fd36
Jacob Dubail <jacob.dubail@10up.com>
2022-08-18 16:03:03 -0700

    resolve conflict in composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,11 +3,7 @@
         'name' => '10up/ca-grants-plugin',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-<<<<<<< Updated upstream
-        'reference' => 'a6ca279c82eb463460ae9e02f37da6f0bda8328b',
-=======
-        'reference' => 'ebf81cab33046f57fdb31b688c609521c9995284',
->>>>>>> Stashed changes
+        'reference' => '6997765efa3776603e909763dd73adda86bb1eb5',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -17,11 +13,7 @@
         '10up/ca-grants-plugin' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-<<<<<<< Updated upstream
-            'reference' => 'a6ca279c82eb463460ae9e02f37da6f0bda8328b',
-=======
-            'reference' => 'ebf81cab33046f57fdb31b688c609521c9995284',
->>>>>>> Stashed changes
+            'reference' => '6997765efa3776603e909763dd73adda86bb1eb5',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
